5c1b9489d01f8ad11bcd6385fecf8650d33600d4
shafi21001 <shafi.msm1999@gmail.com>
2026-07-22 10:19:54 +0600

    first commit

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Tell FastAPI where HTML templates are
+templates = Jinja2Templates(directory="app/templates")
+
+# Serve static files like CSS
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "title": "Hello World",
+            "message": "Welcome to my first FastAPI website!"
+        }
+    )
